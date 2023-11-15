7d1040474471cbce6b71cf0e8c00874dab86bff2
trivernis <trivernis@protonmail.com>
2023-11-15 13:57:02 +0100

    Add plant config validation

--- src/routes/plants/[plant]/+page.ts
+++ src/routes/plants/[plant]/+page.ts
@@ -2,6 +2,8 @@ import { error } from "@sveltejs/kit";
 import type { PageLoad } from "./$types";
 import { plants, type PlantData } from "$lib/plants";
 
+export const prerender = "auto";
+
 export const load: PageLoad = ({ params }) => {
 	if (plants[params.plant]) {
 		return plants[params.plant] as PlantData;
